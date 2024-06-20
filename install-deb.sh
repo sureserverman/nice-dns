5a402a6d5fb047c4062bd682f1bd955a27023ec9
Sure Serverman <host.unmixable219@passinbox.com>
2024-06-20 19:04:29 +0100

    Debian installer fixed and improved

--- install-deb.sh
+++ install-deb.sh
@@ -2,9 +2,6 @@
 
 resconffile="/etc/systemd/resolved.conf"
 
-#Reset firewall settings
-sudo ufw --force reset
-
 #Check if there are installed previous versions
 if [ $(sudo docker ps -a | grep -c "tor-socat\|unbound\|pi-hole") -gt 0 ]
   then
@@ -19,13 +16,12 @@ if [ $(sudo docker ps -a | grep -c "tor-socat\|unbound\|pi-hole") -gt 0 ]
     sudo docker rm tor-socat &>/dev/null
     sudo docker image rm sureserver/tor-socat:latest &>/dev/null
     sudo docker network rm nice-dns-web_dnsnet &>/dev/null
-    sudo ufw disable
-    sudo sed -i -e 's/^DNS/#DNS/g' $resconffile
+    sudo sed -i -e 's/^DNS=/#DNS=/g' $resconffile
     if [[ $(sudo grep "^#DNS=1.1.1.1" $resconffile) = "" ]]
       then
         sudo echo "DNS=1.1.1.1" | sudo tee -a $resconffile
       else
-        sudo sed -i -e 's/^#1.1.1.1/DNS=1.1.1.1/g' $resconffile
+        sudo sed -i -e 's/^#DNS=1.1.1.1/DNS=1.1.1.1/g' $resconffile
       fi
     sudo systemctl restart systemd-resolved
   else
@@ -33,9 +29,18 @@ if [ $(sudo docker ps -a | grep -c "tor-socat\|unbound\|pi-hole") -gt 0 ]
     sudo apt-get install -yq git docker docker-compose
 fi
 
+#Disable stub listener for DNS
+if [[ $(sudo grep "DNSStubListener" $resconffile) = "" ]]
+  then
+    sudo echo "DNSStubListener=no" | sudo tee -a $resconffile
+  else
+    sudo sed -i -e 's/#DNSStubListener/DNSStubListener/g' $resconffile
+    sudo sed -i -e 's/DNSStubListener=yes/DNSStubListener=no/g' $resconffile
+  fi
+
 #Use installed containers ad default DNS-server
 # Comment all DNS server settings
-sudo sed -i -e 's/^DNS/#DNS/g' $resconffile
+sudo sed -i -e 's/^DNS=/#DNS=/g' $resconffile
 
 #Check if there is no line with 127.0.0.1
 if [[ $(sudo grep "^#DNS=127.0.0.1" $resconffile) = "" ]]
@@ -50,16 +55,9 @@ if [[ $(sudo grep "^#DNS=127.0.0.1" $resconffile) = "" ]]
 #Start docker containers
 git clone https://github.com/sureserverman/nice-dns.git
 cd nice-dns
-sudo systemctl stop systemd-resolved
-sudo docker compose up -d
+sudo docker compose --env-file .env up -d
 cd -
 rm -rf nice-dns
 
-# sudo systemctl start systemd-resolved
-
-#Deny in firewall any alternative DNS servers
-sudo ufw default deny incoming
-sudo ufw default allow outgoing
-sudo ufw allow out to 192.168.180.242 port 53
-sudo ufw deny out 53
-sudo ufw --force enable
+#Apply changes to DNS resolver settings
+sudo systemctl restart systemd-resolved
