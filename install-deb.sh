2e4dd653c0bf8264b7de463db80e0ff944f94d2b
Sure Serverman <host.unmixable219@passinbox.com>
2025-05-31 17:15:26 +0100

    Cleaned up legacy

--- install-deb.sh
+++ install-deb.sh
@@ -1,42 +1,35 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-resconffile="/etc/systemd/resolved.conf"
-
 #Check if there are installed previous versions
-if [ "$(sudo docker ps -a | grep -c "tor-socat\|unbound\|pi-hole")" -gt 0 ]
+if [ "$(podman ps -a | grep -c "tor-socat\|unbound\|pi-hole")" -gt 0 ]
   then
     #Remove them if exist
-    sudo docker stop pi-hole &>/dev/null
-    sudo docker rm pi-hole &>/dev/null
-    sudo docker image rm nice-dns-web-pi-hole:latest &>/dev/null
-    sudo docker stop unbound &>/dev/null
-    sudo docker rm unbound &>/dev/null
-    sudo docker image rm nice-dns-web-unbound:latest&>/dev/null
-    sudo docker stop tor-socat &>/dev/null
-    sudo docker rm tor-socat &>/dev/null
-    sudo docker image rm sureserver/tor-socat:latest &>/dev/null
-    sudo docker network rm nice-dns-web_dnsnet &>/dev/null
-    sudo sed -i -e 's/^DNS=/#DNS=/g' $resconffile
-    if [[ $(sudo grep "^#DNS=1.1.1.1" $resconffile) = "" ]]
-      then
-        sudo echo "DNS=1.1.1.1" | sudo tee -a $resconffile
-      else
-        sudo sed -i -e 's/^#DNS=1.1.1.1/DNS=1.1.1.1/g' $resconffile
-      fi
-    sudo systemctl enable systemd-resolved
-    sudo systemctl restart systemd-resolved
+    podman stop pi-hole &>/dev/null
+    podman rm pi-hole &>/dev/null
+    podman image rm nice-dns-web-pi-hole:latest &>/dev/null
+    podman stop unbound &>/dev/null
+    podman rm unbound &>/dev/null
+    podman image rm nice-dns-web-unbound:latest&>/dev/null
+    podman stop tor-socat &>/dev/null
+    podman rm tor-socat &>/dev/null
+    podman image rm sureserver/tor-socat:latest &>/dev/null
+    podman network rm nice-dns-web_dnsnet &>/dev/null
   else
     #Install required software
-    sudo apt-get install -yq git docker docker-compose
+    sudo apt-get install -yq git podman podman-compose
+    ./add-dockerhub.sh
 fi
 
-#Start docker containers
+#Start podman containers
 git clone https://github.com/sureserverman/nice-dns.git
 cd nice-dns
-sudo docker compose --env-file .env up -d
+podman network create \
+  --driver bridge \
+  --subnet 172.31.240.248/29 \
+  dnsnet
+PODMAN_COMPOSE_PROVIDER=podman-compose podman compose --env-file .env up -d
+./persistent-podman.sh
+./dns-deb.sh
 cd -
 rm -rf nice-dns
-
-#Apply changes to DNS resolver settings
-sudo systemctl restart systemd-resolved
