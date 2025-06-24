338970acde0f051a2c10f934c6290a018daafc9a
Sure Serverman <host.unmixable219@passinbox.com>
2025-06-24 22:57:49 +0400

    Small fix

--- deb/dns-deb.sh
+++ deb/dns-deb.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 sudo cp deb/custom-dns-deb.service /etc/systemd/system/custom-dns-deb.service
-chmod +x custom-dns-deb
+chmod +x deb/custom-dns-deb
 sudo cp deb/custom-dns-deb /usr/bin/custom-dns-deb
 sudo systemctl daemon-reload
 sudo systemctl enable --now custom-dns-deb.service
