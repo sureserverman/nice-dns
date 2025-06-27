33553eeab8788e0c3aba3b6adb1a97f0e706129c
Sure Serverman <host.unmixable219@passinbox.com>
2025-06-27 23:03:56 +0400

    ++++

--- mac/mac-rules-persist.sh
+++ mac/mac-rules-persist.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo cp ./local-pf.sh /usr/local/sbin/
-sudo cp ./start-podman.sh /usr/local/sbin/
-sudo cp ./org.localdns.pf.plist /Library/LaunchDaemons/
-sudo cp ./org.startpodman.plist /Library/LaunchDaemons/
+sudo cp ./mac/local-pf.sh /usr/local/sbin/
+sudo cp ./mac/start-podman.sh /usr/local/sbin/
+sudo cp ./mac/org.localdns.pf.plist /Library/LaunchDaemons/
+sudo cp ./mac/org.startpodman.plist /Library/LaunchDaemons/
 
 sudo chmod 644    /Library/LaunchDaemons/org.startpodman.plist
 sudo chmod 644    /Library/LaunchDaemons/org.localdns.pf.plist
