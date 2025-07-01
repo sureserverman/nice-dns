900a55ed9f7b172f4175d67697711081b2ec2b61
Sure Serverman <host.unmixable219@passinbox.com>
2025-07-01 20:37:18 +0400

    +

--- mac/mac-rules-persist.sh
+++ mac/mac-rules-persist.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 launchctl unload ~/Library/LaunchAgents/org.startpodman.plist
 
+sudo launchctl bootout system/org.startpodman || true
+sudo launchctl unload /Library/LaunchDaemons/org.startpodman.plist || true
+sudo rm -f /Library/LaunchDaemons/org.startpodman.plist
+
+
 sudo cp ./mac/start-podman.sh /usr/local/sbin/
 cp ./mac/org.startpodman.plist ~/Library/LaunchAgents/
 
