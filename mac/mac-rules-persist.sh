a794c20938e4b257b2a117bc45d7408315f76740
Sure Serverman <host.unmixable219@passinbox.com>
2025-07-02 17:46:03 +0400

    fix

--- mac/mac-rules-persist.sh
+++ mac/mac-rules-persist.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 launchctl unload ~/Library/LaunchAgents/org.startpodman.plist
 
-sudo launchctl bootout system/org.startpodman || true
-sudo launchctl unload /Library/LaunchDaemons/org.startpodman.plist || true
-sudo rm -f /Library/LaunchDaemons/org.startpodman.plist
+# sudo launchctl bootout system/org.startpodman || true
+# sudo launchctl unload /Library/LaunchDaemons/org.startpodman.plist || true
+# sudo rm -f /Library/LaunchDaemons/org.startpodman.plist
 
 
 sudo cp ./mac/start-podman.sh /usr/local/sbin/
@@ -14,4 +14,4 @@ chmod 644    ~/Library/LaunchAgents/org.startpodman.plist
 # Load it now and at every boot:
 launchctl load ~/Library/LaunchAgents/org.startpodman.plist
 
-/usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate off
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate off
